6b30957486de2f0f56615cf5c7cdfe0a8499b473
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-05-07 14:52:28 -0400

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Andrew Chen on 2021-05-07.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
